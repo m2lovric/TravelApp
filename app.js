9b8cb9d9aafd5e12c8b04eb4f514a7c29b1d23f3
matteo <m2lovric@gmail.com>
2019-03-21 23:22:47 +0100

    slideshow

--- app.js
+++ app.js
@@ -1,4 +1,49 @@
-const destinations = ['#tokyo', '#prague', 'iceland']
+let slideIndex = 1;
+
+slideShow(slideIndex);
+
+function slideControl(n){
+    slideShow(slideIndex += n);
+}
+
+function slideShow (n){
+    let i;
+    const slides = document.getElementsByTagName("article");
+    console.log(slides);
+    if(n > slides.length){
+        slideIndex = 1;
+    } 
+    if (n < 1){
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++){
+        slides[i].style.display = "none"; 
+    }
+    slides[slideIndex-1].style.display = "block"; 
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 showPrague = () => {
     document.querySelector("#tokyo").style.display = "none";
     document.querySelector("#prague").style.display = "block";
