376279e4514d1c0a532953f779c8586b39c49035
matteo <m2lovric@gmail.com>
2019-03-27 01:44:35 +0100

    testing flight api

--- test.js
+++ test.js
@@ -17,17 +17,38 @@ document.querySelector('#locationFrom').addEventListener('input', event => {
     http.send();
 });
 
-let httpfl = new XMLHttpRequest();
+const flyDur = document.querySelector("#duration");
+const fromLocation = document.querySelector("#from");
+const toLocation = document.querySelector("#to");
 
+let httpfl = new XMLHttpRequest();
+let dTime;
+let flyDuration;
 httpfl.onreadystatechange = () => {
     if(httpfl.readyState == 4 && httpfl.status == 200){
         let data = JSON.parse(httpfl.response);
         console.log(data.data);
         console.log(data.data[0].flyFrom);
         console.log(data.data[0].flyTo);
+        let dTime = data.data[0].dTimeUTC;
+        convertTime(dTime);
+        flyDur.innerHTML = data.data[0].fly_duration;
+        fromLocation.innerHTML = data.data[0].cityFrom;
+        toLocation.innerHTML = data.data[0].cityTo;
     }
 }
 
-httpfl.open("GET","https://api.skypicker.com/flights?flyFrom=OSI&to=DUB&dateFrom=18/05/2019&dateTo=25/05/2019&partner=picky", true);
+httpfl.open("GET","https://api.skypicker.com/flights?flyFrom=ZAG&to=MUC&dateFrom=11/04/2019&dateTo=15/04/2019&curr=hrk&sort=date&adults=1&max_stopovers=0", true);
 httpfl.send();
+
+
+//epoch to date
+let dtimeR;
+const timeP = document.querySelector("#time");
+convertTime = (dTime) => {
+    dtimeR = new Date(0);
+    dtimeR.setUTCSeconds(dTime);
+    timeP.innerHTML = dtimeR;
+}
+
 
